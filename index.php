ffaa47032f42e304a9e5347cdc6c36c09fc9c5c2
Henri Tammo <HenriTammo@users.noreply.github.com>
2018-09-10 10:40:48 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- charset on tähestik -->
+  <meta charset="utf-8">
+  <title>Henri Tammo, veebiproge</title>
+</head>
+<body>
+  <body bgcolor="#000000">
+  <font color="green">
+  <h1>Henri Tammo üritus lehekülge teha</h1>
+  <p>Sellest võibolla saab kunagi korrektne lehekülg isegi</p>
+  <p>See leht on valminud <a href="https://www.tlu.ee/" target="_blank"> TLU</a> õppetöö raames.
+  <!-- <a href=""> </a> on lehekülje lingi lisamine. a on ankur. -->
+  <h2>Kodutöö</h2>
+  <p>Olin kunagi bioloog, aga nüüd õpin informaatikat.</p>
+  <!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+  <!--../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" sobib ka pildi panekuks-->
+  <img src="https://upload.wikimedia.org/wikipedia/et/3/34/Vironia_vapp.png" height=800 width=640 alt="TLÜ Terra õppehoone">
+  <!-- kui avada ../ järgi siis on vaja oma greeny lehekülg võtta mitte scp fail-->
+  <p> Link minu kaasüliõpilase lehele <a href="../~kristre/"  target="_blank"> Kristjan Treimann</a>
+</body>
+</html>
